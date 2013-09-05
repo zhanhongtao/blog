8d3655e510657f9c9864918b379ecf9251ec7db9
redky <redky@qq.com>
2013-09-05 10:56:59 +0800

    Revert "合并 master"
    This reverts commit 2a071e6670420447de30e278dd4a83e861393738.

--- scripts/flatArray.js
+++
@@ -1,9 +0,0 @@
-// 函数递归.
-function flatArray( array ) {
-  var result = [];
-  for ( var i = 0, l = array.length; i < l; i++ ) {
-    var current = array[i];
-    result = result.concat( type(current) === 'array' ? flatArray(current) : current );
-  }
-  return result;
-}
