f8d0a70473632212b08474e40f80f8db3c4b3f1b
redky <redky@qq.com>
2013-09-05 10:57:25 +0800

    Revert "Revert "合并 master""
    This reverts commit 441ba3ae2c6fb7788f301de7c7329a09bcc5027b.

---
+++ scripts/after.js
@@ -0,0 +1,20 @@
+
+
+// 调用多少次后, 再执行函数.
+// ex:
+// 多个键做入口.
+function after( count, func ) {
+  var n = ~~count;
+  var old = n;
+  var func = typeof func === 'function' ? func : function() {};
+  var ret = function() {
+    n--;
+    if ( n === 0 ) {
+      func.apply( this, arguments );
+      n = old;
+    }
+    return ret;
+  }
+  return ret;
+}
+
