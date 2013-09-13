8e02038ea97c723e3409bf3b433d4ad8e3e70795
redky <redky@qq.com>
2013-09-13 11:19:13 +0800

    ppt 中添加  queue/sync 实例

--- slide/js/functional.js
+++ slide/js/functional.js
@@ -122,15 +122,11 @@ function after( count, func ) {
 // * 类似 jQuery 中的 queue.
 // * http 相关的单元测试.
 function queue( list, fn, callback, ret ) {
-
     var length = list.length;
-
     // 在 ret 中记录原始长度.
     ret = ret || [ length ];
-
     // 不污染原始数组.
     var old = ret[0] === length ? [].concat(list) : list;
-
     // value -> 当前返回值.
     // stop -> 是否停止运行, 并且只把当前 ret 返回给 callback.
     // returnCurrentValue -> 需要 stop 为真. 只返回当前 value 给 callback.
@@ -142,7 +138,6 @@ function queue( list, fn, callback, ret ) {
         }
         queue( (old.shift(), old ), fn, callback, ret );
     };
-
     if ( length > 0 ) {
         var argus = [ old[0], ret[0] - length, ret.slice(1) ];
         if ( fn.length ) {
