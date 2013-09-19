9ea3fdc463d1df956efb5b426358b1e6d723f543
redky <redky@qq.com>
2013-09-19 16:12:58 +0800

    删除不必要文件

--- scripts/bind.js
+++
@@ -1,9 +0,0 @@
-
-
-var bind = Function.prototype.bind || function ( func, context ) {
-    context = context || this;
-    return function() {
-      return func.apply( context, arguments );
-    }
-};
-
