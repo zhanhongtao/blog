983bb3fce25af20c4033e8bf765eafe2879eb95c
redky <redky@qq.com>
2014-02-13 15:04:29 +0800

    update queue.
    直接对原数组做处理.

--- se/se.js
+++ se/se.js
@@ -248,36 +248,29 @@
     }
     return ret;
   };
-
-  // 队列
-  se.queue = function( list, fn, callback, ret ) {
-      var length = list.length;
-      // 在 ret 中记录原始长度.
-      ret = ret || [ length ];
-      // 不污染原始数组.
-      var old = ret[0] === length ? [].concat(list) : list;
-      // value -> 当前返回值.
-      // stop -> 是否停止运行, 并且只把当前 ret 返回给 callback.
-      // returnCurrentValue -> 需要 stop 为真. 只返回当前 value 给 callback.
-      var next = function ( value, stop, returnCurrentValue ) {
-          ret[ ret.length ] = value;
-          if ( stop ) {
-              callback.apply( null, returnCurrentValue ? [value] : ret.slice(1) );
-              return;
-          }
-          se.queue( (old.shift(), old ), fn, callback, ret );
-      };
-      if ( length > 0 ) {
-          var argus = [ old[0], ret[0] - length, ret.slice(1) ];
-          if ( fn.length ) {
-              argus = argus.slice( 0, fn.length - 1 );
-          }
-          argus.push( next );
-          fn.apply( null, argus );
+  
+  se.queue = function( list, fn, callback, index, ret ) {
+    "use strict";
+    index = index || 0;
+    ret = ret || [];
+    var next = function ( value, stop, returnCurrentValue ) {
+      ret[ ret.length ] = value;
+      if ( stop ) {
+        return callback.apply( null, returnCurrentValue ? [value] : ret );
       }
-      else if ( callback && ret.shift() === ret.length ) {
-          callback.apply( null, ret );
+      se.queue( list, fn, callback, ++index, ret );
+    };
+    if ( index < list.length ) {
+      var argus = [ list[ index ], index, ret ];
+      if ( fn.length ) {
+        argus = argus.slice( 0, fn.length - 1 );
       }
+      argus.push( next );
+      fn.apply( null, argus );
+    }
+    else if ( callback ) {
+      callback.apply( null, ret );
+    }
   };
 
   // 同步, 提前把结果抛出.
