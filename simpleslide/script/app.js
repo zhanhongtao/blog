f07d075ca8a1f8924b7dbafd831952bc0c514edd
redky <redky@qq.com>
2014-08-10 14:42:57 +0800

    对 slide 添加 step/per 支持.

--- simpleslide/script/app.js
+++ simpleslide/script/app.js
@@ -2,11 +2,11 @@ var default_item_width = 240;
 var box = document.getElementById( 'box' );
 var helper = document.getElementById( 'box-helper' );
 var list = box.getElementsByTagName( 'li' );
+helper.style.width = list.length * default_item_width + 'px';
 
 var myslide = slide({
-  auto: false,
-  timeout: 2,
   max: list.length,
+  rotate: true,
   onchange: function( to, from, oto ){
     var i = 0;
     while ( i < 2 ) {
@@ -22,22 +22,72 @@ var myslide = slide({
     }
     // 直接使用 css3 的 transition 实现.
     helper.style.marginLeft = -1 * to * default_item_width + 'px';
-    /*!
-    if ( from !== to ) {
-      var start = from * default_item_width;
-      var end = to * default_item_width;
-      simple( 250, 'easeOut', function( p ) {
-      helper.style.marginLeft = -1 * start + ( start - end ) * p + 'px';
-      });
-    }
-    */
   }
 });
 
-document.getElementById( 'prev' ).onclick = function() {
-  myslide.prev();
-};
+var btns = box.getElementsByTagName( 'button' );
+for ( var i = 0, l = btns.length; i < l; ++i ) {
+  btns[i].onclick = function() {
+    var className = this.className;
+    if ( className === 'next' ) {
+      myslide.next();
+    }
+    else if ( className == 'prev' ) {
+      myslide.prev();
+    }
+  };
+}
 
-document.getElementById( 'next' ).onclick = function() {
-  myslide.next();
-};
+
+var instance = document.getElementById( 'instance' );
+var instanceHelper = document.getElementById( 'instance-helper' );
+var instanceList = instance.getElementsByTagName( 'li' );
+var finily_item_width = default_item_width + 5;
+var per = 2;
+instance.style.width = default_item_width * per + 'px';
+instanceHelper.style.width = instanceList.length * finily_item_width + 'px';
+
+var rslide = slide({
+  auto: true,
+  timeout: 2,
+  rotate: false,
+  step: 2,
+  per: per,
+  max: instanceList.length,
+  onchange: function( to, from, oto ) {
+    // oto/from 确认方向.
+    var i = 0;
+    while ( i < per ) {
+      var index = to + i;
+      if ( index < instanceList.length ) {
+        var img = instanceList[index].getElementsByTagName( 'img' )[0];
+        if ( !img.loaded ) {
+          img.src = img.getAttribute( 'data-src' );
+          img.loaded = 1;
+        }
+      }
+      ++i;
+    }
+    
+    if ( from !== to ) {
+      var start = from * finily_item_width;
+      var end = to * finily_item_width;
+      simple( 350, 'easeOut', function( p ) {
+        instanceHelper.style.marginLeft = -1 * start + ( start - end ) * p + 'px';
+      });
+    }
+  }
+});
+
+var btns = instance.getElementsByTagName( 'button' );
+for ( var i = 0, l = btns.length; i < l; ++i ) {
+  btns[i].onclick = function() {
+    var className = this.className;
+    if ( className === 'next' ) {
+      rslide.next();
+    }
+    else if ( className == 'prev' ) {
+      rslide.prev();
+    }
+  };
+}
--- simpleslide/script/slide.js
+++ simpleslide/script/slide.js
@@ -1,9 +1,4 @@
 ;(function( global ) {
-
-  // 空函数.
-  function noop() {};
-
-  // 合并对象.
   function extend( a ) {
     var argus = [].slice.call( arguments, 1 );
     for ( var i = 0, l = argus.length; i < l; ++i ) {
@@ -17,34 +12,33 @@
     return a;
   }
 
-  // 修正索引.
-  function fixIndex( index, max, rotate ) {
-    return rotate ?
-        ( index % max + max ) % max :
-      index >= max ? max - 1 :
-        index < 0 ? 0 : index;
+  function fixIndex( index, config ) {
+    var max = config.max, rotate = config.rotate, 
+        per = config.per, stop = config.stop;
+    index = rotate ? ( index % max + max ) % max :
+      index >= max ? max - 1 : index < 0 ? 0 : index;
+    return stop ? ( index + per >= max ? max - per : index ) : index;
   }
-
 
   // 全局对象.
   var slide = global.slide = function( config ) {
-    if ( !(this instanceof slide) ) {
-      return new slide( config );
-    }
+    if ( !(this instanceof slide) ) return new slide( config );
     var config = this.config = extend( {}, slide.default_config, config );
     this.index = config.index;
-    this.onchange = config.onchange;
     this.playing = !!config.auto;
+    this.onchange = config.onchange;
     this.init();
   };
 
-  // 默认配置.
   slide.default_config = {
-    index: 0,
-    max: 5,
-    rotate: true,
-    auto: false,
-    timeout: 3
+    index: 0,               // 默认索引
+    max: 5,                 // 屏个数
+    rotate: true,           // 是否 rotate
+    auto: false,            // 是否自动切换
+    timeout: 3,             // 自动切换时, 时间间隔
+    per: 1,                 // 每屏显示个数
+    step: 1,                // 每次切换步长 - @NOTE: 显示个数可能会和步长不同
+    stop: true              // 不够 per 显示时, stop 处理.
   };
 
   // 初始化.
@@ -57,43 +51,44 @@
     if ( to == null ) return this;
     from = from == null ? this.index : from;
     var config = this.config;
-    this.index = fixIndex( to, config.max, config.rotate );
+    this.index = fixIndex( to, config );
     if ( this.timer ) clearTimeout( this.timer );
-    if ( typeof this.onchange === 'function' ) this.onchange( this.index, from, to );
-    if ( this.playing ) { 
+    if ( typeof this.onchange === 'function' ) this.onchange.call( this, this.index, from, to );
+    if ( this.playing ) {
       var self = this;
       this.timer = setTimeout( function(){
         self.next();
-      }, config.timeout * 1000 ); 
+      }, config.timeout * 1000 );
     }
     return this;
   };
 
   // 下一屏.
   slide.prototype.next = function() {
-    return this.go( this.index + 1, this.index );
+    return this.go( this.index + this.config.step, this.index );
   };
 
   // 上一屏.
   slide.prototype.prev = function() {
-    return this.go( this.index - 1, this.index );
+    return this.go( this.index - this.config.step, this.index );
   };
 
-  // 取/设置配置信息.
-  slide.prototype.config = function( config ) {
-    return config ? extend( this.config, config ) : this.config;
-  };
- 
   // 停止自动切屏.
   slide.prototype.pause = function() {
     this.playing = false;
+    if ( this.timer ) {
+      clearTimeout( this.timer ), this.timer = null;
+    }
     return this;
   };
- 
+
   // 继续自动切屏.
   slide.prototype.resume = function() {
+    var self = this, config = this.config;
     this.playing = true;
-    this.next();
+    this.timer = setTimeout( function(){
+      self.next();
+    }, config.timeout * 1000 );
     return this;
   };
 
