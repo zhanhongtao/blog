3ad41147fc576395c396238531c6c686edeec570
redky <redky@qq.com>
2014-12-08 21:06:50 +0800

    记录 CSS.

--- index/index.js
+++ index/index.js
@@ -34,29 +34,41 @@ var blog = {
     {title: 'Lottery', address: './lottery/', description: '抽奖小程序' },
     {title: 'expand-select', address: './expandselect/', description: '可使用 shift 辅助选择'},
     {title: 'css 之 desktop', address: './desktop/'},
-    {title: 'Ajax', address: './fe/ajax.txt', description: 'Ajax 相关文档' },
-    {title: 'selctor', address: './fe/selector/' },
+    {title: 'Ajax', address: './fe/ajax.txt', description: '文档' },
     {title: 'rt.js', address: 'https://github.com/zhanhongtao/rt.js', description: '基于 JavaScript 语法的模板引擎' },
     {title: 'jQuery.slide.js', address: 'http://github.com/zhanhongtao/jQuery.slide.js', description: '可定制化 jQuery slide 插件' },
-    {title: 'ppt', address: './ppt' }
+    {title: 'PPT', address: './ppt' }
   ],
   demos: [
-    {title: 'scroll loading..', address: './fe/scroll/' },
-    {title: 'sticky', address: './fe/scroll/sticky.html'},
+    {title: 'Selctor', address: './fe/selector/' },
+    [
+      {title: '最小高度', address: './fe/sticky-link-bottom.html', description: 'vh + calc 版本'}, 
+      {title: '兼容最小高度', address: './fe/sticky-like-bottom-compatible.html', description: 'html 100% 版本'}
+    ],
+    [
+      {title: 'Scroll Loading..', address: './fe/scroll/' },
+      {title: 'Sticky', address: './fe/scroll/sticky.html'}
+    ],
     {title: '遍历 DOM-Tree', address: './walkdomtree/'},
-    {title: 'queue', address: './queue/', description: 'es3, Yield, Promise, CSS3' },
-    {title: 'border', address: './fe/border.html'},
-    {title: 'border-radius', address: './fe/border-radius.html'},
-    {title: 'Style About Box', address: './fe/box-style.html'},
-    {title: 'flex', address: './fe/flex/'},
-    {title: 'box', address: './fe/box.html'},
-    {title: 'gradient', address: './fe/gradient.html'},
-    {title: 'layout - css', address: './layout/', description: 'alt + Number 切换 css 文件)' },
-    {title: 'css - Animation', address: './fe/animation.html' },
-    {title: 'css - Auto Transition', address: './fe/auto-transition.html', description: '自动 transition' },
+    {title: 'Queue', address: './queue/', description: 'es3, Yield, Promise, CSS3' },
+    [
+      {title: 'Style About Box', address: './fe/box-style.html'},
+      {title: 'Border', address: './fe/border.html'},
+      {title: 'Border-radius', address: './fe/border-radius.html'}      
+    ],
+    [
+      {title: 'Box', address: './fe/box.html'},
+      {title: 'Flex', address: './fe/flex/'}      
+    ],
+    {title: 'CSS - Gradient', address: './fe/gradient.html'},
+    {title: 'CSS layout', address: './layout/', description: 'Alt + Number 切换 css 文件)' },
+    [
+      {title: 'CSS - Animation', address: './fe/animation.html' },
+      {title: 'JavaScript - Animation', address: './fe/animation-javascript.html', description: '需要浏览器支持' }
+    ],
+    {title: 'CSS - Auto Transition', address: './fe/auto-transition.html', description: '自动 transition' },
     {title: 'simple-slide', address: './simpleslide/', description: '支持 Touch 事件' },
-    {title: 'Object - JavaScript', address: './fe/object.txt', description: 'JavaScript 中 Object 方法/属性文档'},
-    {title: 'JavaScript - Animation', address: './fe/animation-javascript.html', description: '需要浏览器支持' }
+    {title: 'Object - JavaScript', address: './fe/object.txt', description: 'JavaScript 中 Object 方法/属性文档'}
   ]
 };
 
