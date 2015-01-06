9e3994ee1753875b2ebfee5b0e504048c7ae11f7
redky <redky@qq.com>
2015-01-06 15:13:58 +0800

    更新 11 号日志

---
+++ log/2015-01-11.js
@@ -0,0 +1,74 @@
+/*
+  @ittce
+  http://ejohn.org/apps/learn/
+
+  # html/css
+  * html
+    html 语义化
+    html class/id 命名
+    form: https://html.spec.whatwg.org/multipage/forms.html#forms
+
+  * css
+    选择器/权重
+    display
+      - inline/block/inline-block/none
+      - flex/box
+      - table/table-cell
+    盒子模型
+      - margin/border/padding/width/height
+      - outline/box-shadow/box-sizing
+      - resize/overflow
+      - margin 合并
+      - margin 百分比
+      - margin 负值
+      - border-radius
+      - border-image (可选)
+      - border 三角
+      - min-width/min-height
+      - max-width/max-height
+    定位
+      - position - static/relative/absolute/fixed
+      - top/right/bottom/left
+      - z-index
+      - clip
+      - opacity
+      - visibility
+    浮动
+      - float
+      - clear[clearfix]
+      - zoom
+      - :before/:after
+      - content
+      - BFC
+    行内(inline)
+      - font: style/size/weight/family/stretch
+      - text-align
+      - vertical-align
+      - line-height
+      - text-indent
+      - text-decoration
+      - text-shadow
+      - text-overflow
+      - white-space
+      - direction: ltr/rtl
+    样式
+      - background[color/image/repeat/position/size] (multiple-backgrounds)
+      - color - rgba
+      - cursor
+      - resize
+      - user-select
+      - gradient
+    动画
+      - transform/transform-origin
+      - transition[property/duration/timing-function/delay]
+      - animation[name/duration/timing-function/delay/iteration-count/direction/play-state/fill-mode]
+      - will-change
+
+    习题(效果)
+      ...
+
+    推荐博客:
+    http://www.zhangxinxu.com/wordpress/
+
+*/
+
