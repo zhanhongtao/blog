92bf4a2e6b613600e3e873a3e799604da8c75222
redky <redky@qq.com>
2015-01-13 08:59:46 +0800

    更新 Heart 透明效果

--- log/2015-01-11.js
+++ log/2015-01-11.js
@@ -1,6 +1,25 @@
 /*
   @ittce
   http://ejohn.org/apps/learn/
+  1. 完善 Download 插件
+    * 去处 jQuery 依赖, 直接操作 DOM 节点
+    * 尝试支持探索迅雷下载链接
+    * 把探索到的结果整理, 放到一个层中
+  2. event/slide/animation 整理, 放到 github
+    * 熟悉 github 项目中目录结构
+    * 补充文档
+    * 熟悉版权
+  3. 复习 jQuery 插件书写结构, 即怎么扩展一个类/对象
+  4. 复习各种排序算法
+    * 插入排序
+    * 归并排序
+    * 快速排序
+    * 堆排序
+    * 熟悉: 冒泡排序/选择排序/桶排序
+  5. XSS
+    * HTML/CSS/JavaScript 中字符串表示方式
+    * HTML 中 xss 插入点, 以及应该怎么处理
+  6. 了解(百度)搜索建议, 熟悉它都有哪些操作细节 - 尝试写一个 [DOM]
 
   # html/css
   * html
---
+++ log/others.js
@@ -0,0 +1,23 @@
+
+
+/*
+  words:
+  sort: quickSort, heapSort, mergeSort, selectSort
+  git ignore
+  extension
+  operator
+  config
+  include exclude
+  global local
+  system
+
+  upwards/downwards
+  backwards/forwards
+
+  syntax
+  plugin
+  indent
+
+  dark/light
+*/
+
