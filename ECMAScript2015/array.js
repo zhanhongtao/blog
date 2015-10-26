d212ad8481b382fef282e4bd009e1d3ee19e0e94
zhanhongtao <redky@qq.com>
2015-10-26 10:07:59 +0800

    完善 array 部分

--- ECMAScript2015/array.js
+++ ECMAScript2015/array.js
@@ -15,35 +15,58 @@
   Array.prototype.includes(value[, startIndex]);
 */
 
-
 // https://googlechrome.github.io/samples/array-methods-es6/index.html
 
+// 添加 Array.isArray 静态方法以后,
+// 新加的静态方法 -
+// Array.from/Array.of
+
+// Array.from 可用于伪数组(.length), NodeList, 具有 iterable 性质的对象
 // 不再需要 [].slice.call 方法
+// Array.from(array) 是浅 copy 操作.
+
 // string - 内置拆分算法, 修正多字节问题
 Array.from('abc'); // -> ['a', 'b', 'c']
+
 // NodeList
 Array.from(document.links); // [node, node]
 Array.from(document.querySelectorAll('a'))
-// map
-Array.from(document.links, (a) => a.href); // [href, href]
+
 // arguments
 var t = (...all) => Array.from(all);
 t(1, 2, 3, 4, 5); // [1, 2, 3, 4, 5]
-// array 没必要
+
+// array
+// 浅复制
 Array.from([1, 2, 3]);
+
+// 支持具有 iterable 性质数据结构
 // set
 var set = new Set();
 set.add(1);
 Array.from(set);
+
 // map
 var map = new Map;
 map.set('a', 'A');
 Array.from(map); // [ ['a', 'A'] ]
+
+// 伪数组
 // object
 Array.from({a: 'a'}); // []
 Array.from({a: 'a', length: 1}); // []
 Array.from({0: 'a'}); // []
 Array.from({0: 'a', 10: 'A', length: 11}); // ['a', ......., 'A']
+
+// 使用扩展运算符也可以转换一些数据结构
+// 扩展运算符依赖 Symbol.iterator
+[...document.querySelectorAll('div')]
+
+// 支持第二个参数, map!
+Array.from(document.querySelectorAll('a'), (node) => node.href);
+
+// 支持第三个参数, context.
+// 是 map 中的 this!
 
 
 // 修正 new Array 不足
@@ -56,6 +79,7 @@ Array.of('a', 'b');
 // 补充 indexOf 函数
 var tmp = [1, 2, 3, null, NaN, true, false];
 tmp.indexOf(NaN); // -1
+
 // 找到返回索引;
 // 反之, 返回 -1;
 tmp.findIndex((value, index, array) => isNaN(value));
@@ -85,6 +109,7 @@ tmp.find((v) => {
 
 
 // 填充值
+// 语法: .fill(value, start, end)
 new Array(10).fill(8);
 new Array(10).fill(8, 0, 5);
 new Array(10).fill(8, 5, -1);
@@ -94,7 +119,11 @@ new Array(10).fill(8, 5, -1);
 [1, 2, 3, 4, 5].slice(1, 4); // [2, 3, 4]
 
 
+// 会直接修改原数组
 // copyWithin: 复制 start, end 区间元素到 target 位置
+// 语法: copyWithin(target, start, end);
+// start[0]/end[length] 支持负数
+
 // 复制 0、1 两个元素, 粘贴到 3、4 位置
 [0, 1, 2, 3, 4, 5].copyWithin(3, 0, 2); // [0, 1, 2, 0, 1, 5]
 // 同上, 但复制的元素个数多 1
@@ -110,6 +139,7 @@ new Array(10).fill(8, 5, -1);
 [0, 1, 2, 3, 4, 5].copyWithin(0, 3);  // [3, 4, 5, 3, 4, 5]
 
 
+// 迭代器
 for (let index of ['a', 'b'].keys()) {
   console.log(index); // 0, 1
 }
