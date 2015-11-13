b92c3cb91663947e5b0b16dfa73082c76e2dfef5
zhanhongtao <redky@qq.com>
2015-11-13 10:37:47 +0800

    Add DEMO

--- ECMAScript2015/class.js
+++ ECMAScript2015/class.js
@@ -11,6 +11,7 @@
 class Point {
   // 使用自定义 constructor 函数.
   // 当不存在 constructor 函数时, 默认会使用 `constructor() {}` 代替
+  // 相当于 ES5 中的 function Point.
   constructor(x, y) {
     this.x = x;
     this.y = y;
@@ -68,14 +69,19 @@ cp.toString();
 // typeof Point -> function
 // 但是不能再把 Point 当做函数使用了.
 // 只能使用 new 关键字
+// 可以认为修正 ES5 中, 构造函数依然可当作函数调用的问题
+// 默认返回 this.
+// 和 ES5 相同, 可手动指定返回其它对象.
 
 // class 声明的类, 不能挂起.
 // 新声明都不支持了 - let/const/class!
 
 // class 支持表达式写法.
 // 类名和 ES5 中的函数名一样, 只能内部使用.
+// 可以写立即执行的 class.
 
 // class 代码块内部只支持方法, 不支持属性.
+// 内容定义的方法是不可枚举的 - un enumerable - 和 ES5 不同.
 
 // 静态方法和原型链方法
 // 同时支持表达式做方法名.
@@ -92,9 +98,14 @@ class Foo {
 // 支持 * 符号 - 迭代器方法
 
 // 静态方法也会被继承.
+// 同时可在子类中使用 super.staticMethod() 方式调用
+
+// new 关键字存在 target 属性.
+// 当使用函数调用方式执行构造函数时, new.target 为 undefined.
+// 这种方法可定位用户调用方式.
+
+
 
 // 参考:
 // http://www.2ality.com/2015/02/es6-classes-final.html
 // https://github.com/GoogleChrome/samples/blob/gh-pages/classes-es6/index.html
-
-
--- ECMAScript2015/function.js
+++ ECMAScript2015/function.js
@@ -1,12 +1,16 @@
 'use strict';
 
-// 函数参数默认值
-// 当函数参数完全等于 undefined 时, 才会使用默认值.
+// 函数参数默认值 - 当函数参数严格等于 undefined 时, 才会使用.
 /*!
   1. 当存在默认值时, 形式函数长度(func.length) 表示的是, 不存在默认值的参数个数.
-  2. 默认值为函数时, 函数作用域的是当前函数作用域.
+  2. 默认值为函数时, 函数作用域的是当前函数作用域 - 即在函数定义时, 会被解析
   3. 不能在函数内部再次使用 let/const 声明.
 */
+
+// 2015/11/13: 可在 firefox 下运行.
+function dox(x = 1) {}
+console.log(dox.length); // 0
+
 
 // 可查看相关demo.
 // http://github.com/GoogleChrome/samples
@@ -23,12 +27,13 @@ test.do();
 let test = {
   do() {
     console.log(this); // {}
-  }  
+  }
 };
 test.do();
 
 
 // 匿名函数
+// 箭头函数中, 没有参数或参数个数大于1时, 括号不能省略.
 ;(()=>{
   'use strict';
   let a = 'test';
@@ -39,7 +44,7 @@ test.do();
 ;(function() {
   'use strict';
   let f = (...x) => x.map(v => 2 * v);
-  f(1, 2, 3); // [2, 4, 5]
+  f(1, 2, 3); // [2, 4, 6]
 })();
 
 // DEMO
@@ -89,6 +94,146 @@ Math.max(...[14, 3, 77]);
 // let a = () => (f() , g());
 // f 不算尾位置, 而 g 算.
 
+
+// DEMO.
+// http://perfectionkills.com/javascript-quiz-es6/
+// 1.
+(function(x, f = () => x) {
+  var x;
+  var y = x;
+  x = 2;
+  return [x, y, f()]; // [2, 1, 2]
+})(1)
+
+// 2.
+(function() {
+  return [
+    (() => this.x).bind({ x: 'inner' })(),  // outer.  bind 对箭头函数无效
+    (() => this.x)()                        // outer
+  ]
+}).call({ x: 'outer' });
+// 使用箭头函数指定 this 后, 不能再改变
+// 使用 call/apply/bind 都无效.
+
+// 3.
+let x, { x: y = 1 } = { x };
+y;                                          // 1 默认值
+/**
+  let x;
+  let {x: y = 1} = {x}
+  // ->
+  let {x: y = 1} = {x: x}
+  // ->
+  let {x: y = 1} = {x: undefined}
+  // 当 value 为 undefined 时, 使用默认值
+*/
+
+// 4.
+(function() {
+  // 在严格模式下, this 为 undefined
+  // let f = class h {}
+  let f = this ? class g { } : class h { };
+  return [
+    typeof f, // function. ES6 中的 class 更像语法糖, 本质还是构造函数.
+    typeof h  // undefined. 在表达式中使用类/函数时, 类/函数名称只能内部使用
+  ];
+})();
+
+// 5.
+(typeof (new (class { class () {} }))) // object.
+/*!
+  class {
+    // 构造函数默认返回 this.
+    class() {
+
+    }
+  }
+  typeof new class;
+  // ES5 中, 已允许关键字做属性.
+*/
+
+// 6.
+typeof (new (class F extends (String, Array) { })).substring
+/*!
+  class F extends (String, Array) {
+
+  }
+  // ->
+  class F extends Array {}
+
+  let value = new F;
+  typeof value.substring // undefined.
+*/
+
+// 7.
+[...[...'...']].length
+/**
+  // 扩展运算符
+  var a = [...'...']; // ['.', '.', '.']
+  var b = [...a];     // ['.', '.', '.']
+  b.length;           // 3
+*/
+
+// 8.
+typeof (function* f() { yield f })().next().next()
+/**
+  function* f() {
+    yield f
+  }
+  let g = f();
+  g.next()
+  // object...
+  g.next() // Error
+*/
+
+// 9.
+typeof (new class f() { [f]() { }, f: { } })[`${f}`]
+/**
+  t = new class f() { // 这里语法错误. class f {}
+    [f]() {},
+    f: {}
+  }
+  v = t[`${f}`]
+  typeof v;
+*/
+
+// 10.
+typeof `${{Object}}`.prototype  // 'undefined'
+/**
+  typeof `{Object}`.prototype;
+  typeof '[object Object]'.prototype;
+  typeof undefined; // 'undefined'
+*/
+
+// 11.
+((...x, xs)=>x)(1,2,3) // Error
+// ...x 表示剩余参数, 也就是说只能放在最后面
+
+// 12.
+let arr = [ ];
+for (let { x = 2, y } of [{ x: 1 }, 2, { y }]) {
+  arr.push(x, y);
+}
+arr;
+// Error
+
+// 13.
+(function() {
+  if (false) {
+    let f = { g() => 1 };
+  }
+  return typeof f;
+})()
+// Error
+
+// 14.
+(function () {
+  // this -> {x: 10}
+  // arguments -> [20]
+  // 箭头函数的 context 为 {x: 10}
+  // 箭头函数内部不能再使用自身 arguments
+  return (y) => console.log(this.x + arguments[0] + y);
+}).call({x: 10}, 20)(30); // 60
+
 // 参考:
 // http://www.2ality.com/2015/06/tail-call-optimization.html
-
