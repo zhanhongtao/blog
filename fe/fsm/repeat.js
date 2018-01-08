41ddd4396ede2ba803d5505e49264d94e5974863
redky <redky@qq.com>
2018-01-08 15:09:53 +0800

    update repeat

--- fe/fsm/repeat.js
+++ fe/fsm/repeat.js
@@ -1,13 +1,19 @@
-function repeat (times, req, next) {
-  return (...args) => new Promise((resolve, reject) => (function repeat (time, times) {
+export default function repeat (times, req, next) {
+  return (...args) => new Promise((resolve, reject) => (function _repeat (time, times) {
     if (time <= times) {
-      req(...args, time)
-        .then(resolve)
-        .catch(e => next(e, time, times))
-        .then(resolve)
-        .catch(() => repeat(++time, times))
+      let p = req(...args, time)
+      p.then(resolve).catch(uncaughtmessage)
+      let n = p.catch(e => next(e, time, times))
+      n.then(resolve).catch(uncaughtmessage)
+      n.catch(() => _repeat(++time, times))
     } else {
       reject(new Error('maxTimes'))
     }
   })(1, times))
+}
+
+function uncaughtmessage (e) {
+  if (process.env.development) {
+    console.warn(`repeat: ${e.message}`)
+  }
 }
